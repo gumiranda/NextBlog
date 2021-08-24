1d3a460a4ef6ababf21e844ec9bc43de7e1b4ae8
Gustavo <gustavo.aguiar@redspark.io>
2021-08-24 13:49:15 -0300

    revert

--- src/components/hero-post.tsx
+++ src/components/hero-post.tsx
@@ -1,6 +1,6 @@
 import Avatar from "./avatar";
 import DateFormatter from "./date-formatter";
-//import CoverImage from "./cover-image";
+import CoverImage from "./cover-image";
 import Link from "next/link";
 import Author from "../types/author";
 import { useFetch } from "lib/fetcher";
@@ -22,9 +22,9 @@ const HeroPost = ({ title, coverImage, date, excerpt, author, slug }: Props) => 
 
   return (
     <section>
-      {/* <div className="mb-8 md:mb-16">
+      <div className="mb-8 md:mb-16">
         <CoverImage title={title} src={coverImage} slug={slug} />
-      </div> */}
+      </div>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
