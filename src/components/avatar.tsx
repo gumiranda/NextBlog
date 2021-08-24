debc4fe034bd6cba6f71986711b606e5de59353e
Gustavo <gustavo.aguiar@redspark.io>
2021-08-24 13:54:05 -0300

    fix

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type Props = {
   name: string;
   picture: string;
@@ -8,7 +6,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <Image src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -1,6 +1,5 @@
 import cn from "classnames";
 import Link from "next/link";
-import Image from "next/image";
 
 type Props = {
   title: string;
@@ -10,7 +9,7 @@ type Props = {
 
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn("shadow-small", {
