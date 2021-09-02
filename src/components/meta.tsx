12a0a887ba6f6172c7af5c7fc1492dbb93d7ed5e
Gustavo <gustavo.aguiar@redspark.io>
2021-09-02 10:22:53 -0300

    dssdds

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -22,11 +22,11 @@ const Meta = () => {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#7159c1" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#7159c1" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#7159c1" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
