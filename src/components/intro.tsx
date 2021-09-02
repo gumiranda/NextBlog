515a0561a7c2f69b58e597e050c65d2c0bfda8b3
Gustavo <gustavo.aguiar@redspark.io>
2021-09-02 10:06:09 -0300

    saassa

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,13 +1,11 @@
-import { CMS_NAME } from "lib/constants";
-
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog personalizado
+        Dev Doido
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Confira seus projetos e portfólios.
+        Programador é tudo doido! E eu não sou diferente, bora aprender React!
       </h2>
     </section>
   );
