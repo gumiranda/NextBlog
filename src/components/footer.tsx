8b3e2d98c6091ae63bb56ccd61eb2d93cc9c13c6
Gustavo <gustavo.aguiar@redspark.io>
2022-10-13 11:13:26 -0300

    a

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,6 +21,12 @@ const Footer = () => {
             >
               Meu GitHub
             </a>
+            <a
+              href="https://crazystack.com.br"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+            >
+              CrazyStack
+            </a>
           </div>
         </div>
       </Container>
