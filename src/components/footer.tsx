5d8eee92bcae377746aa2d95c7040538aaa8d5a2
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-13 11:14:30 -0300

    .

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,12 +10,6 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://www.youtube.com/c/DevDoido/videos"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Canal do Youtube
-            </a>
-            <a
               href={`https://github.com/gumiranda`}
               className="mx-3 font-bold hover:underline"
             >
